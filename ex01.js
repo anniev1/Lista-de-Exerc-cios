4f83b9d67030d4f2566085e813eb32584247c02b
annie <annie_marie@estudante.sesisenai.org.br>
2026-03-23 12:46:01 -0300

    adf

---
+++ ex01.js
@@ -0,0 +1,16 @@
+function somaNumeros() {
+    let soma = 0;
+    let continuar = true;
+    while (continuar) {
+        let numero = parseFloat(prompt("Digite um número (ou 'sair' para encerrar):"));
+        
+        if (isNaN(numero)) {
+          //soma= soma + numero
+          console.log(soma);
+        } else {
+            alert(" Por favor, digite um numero válido!"); 
+        }
+        continuar = confirm("Deseja continuar somando números?");
+        }
+        alert ("A soma total é: " + soma);
+    }
